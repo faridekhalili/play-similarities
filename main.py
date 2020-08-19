be6be391c41b1cbc13d056fc38dcac0a1ec8b626
Faride <faridekhalili1393@gmail.com>
2020-08-19 18:46:26 +0430

    add print message and update depth to 1000

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ class Forest():
     def bfs(self):
         while not self.bfs_queue.empty():
             node, seed = self.bfs_queue.get()
-            if App.select().where(App.seed == seed).count() >= self.depth:
+            app_cnt = App.select().where(App.seed == seed).count()
+            if app_cnt >= self.depth:
+                print('continuing to next seed with {} apps gathered for {}'.format(app_cnt, seed))
                 continue
             details = app_details(node)
             if not details or not len(details):
@@ -35,7 +37,7 @@ def main():
     conf = toml.load('config.toml')
     db.connect()
     db.create_tables([App, Similarity])
-    forest = Forest(conf['seed_apps'], depth=90)
+    forest = Forest(conf['seed_apps'])
     forest.bfs()
     print('%d apps gathered' % App.select().count())
     print('%d similarities gathered' % Similarity.select().count())
