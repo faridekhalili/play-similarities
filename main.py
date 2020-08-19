7109aee2c2458425704e20285dc6709be2c8a9a2
Faride <faridekhalili1393@gmail.com>
2020-08-19 16:56:14 +0430

    refactor code

--- main.py
+++ main.py
@@ -21,13 +21,22 @@ file_handler.setFormatter(formatter)
 root.addHandler(handler)
 root.addHandler(file_handler)
 
-attribute_error_list = []
+
+def get_app_details(app, seed, counter):
+    app_detail = play_scraper.details(app['app_id'])
+    defaults = {
+        'category': app_detail['category'],
+        'score': app_detail['score'],
+        'seed': seed,
+        'description': app_detail['description'],
+        'row_number': counter
+    }
+    return defaults
 
 
-def write_attribute_error_list(AttributeErrorCounts):
-    with open(AttributeErrorCounts, 'w') as file:
-        for attribute_error_cnt in attribute_error_list:
-            file.write("%i\n" % attribute_error_cnt)
+def app_not_already_exists(app):
+    query = App.select().where(App.app_id == app['app_id'])
+    return not query.exists()
 
 
 def some_new_app(count, prev_count):
@@ -53,83 +62,77 @@ def insert_similar(app, similar):
     )
 
 
-def insert_similar_apps(app, counter, seed, app_cnt, attr_error_cnt):
+def insert_similar_apps(app, seed, app_cnt, counter):
     while True:
         try:
             similar_apps = play_scraper.similar(app['app_id'], detailed=False)
             for similar in tqdm(similar_apps, position=1):
-                app_cnt, counter = insert_app(similar, counter, seed, app_cnt)
+                app_cnt, counter = insert_app(similar, seed, app_cnt, counter)
                 insert_similar(app, similar)
             break
         except AttributeError:
             root.error("Fetching similar apps for %s failed, AttributeError" % app['app_id'])
-            attr_error_cnt += 1
             break
         except (ReadTimeout, ConnectionError):
             root.warning("ReadTimeout error, waiting for 5 seconds.")
             time.sleep(5)
-    return app_cnt, counter, attr_error_cnt
-
-
-def insert_app(app, counter, seed, app_cnt):
-    query = App.select().where(App.app_id == app['app_id'])
-    if not query.exists():
-        app = play_scraper.details(app['app_id'])
-        data, created = App.get_or_create(
-            app_id=app['app_id'],
-            defaults={
-                'category': app['category'],
-                'score': app['score'],
-                'seed': seed,
-                'description': app['description'],
-                'row_number': counter
-            }
-        )
-        if created:
-            app_cnt += 1
-            counter += 1
     return app_cnt, counter
 
 
-def one_step_bfs(root_app, counter, seed, app_cnt, attr_error_cnt):
-    app_cnt, counter = insert_app(root_app, counter, seed, app_cnt)
-    app_cnt, counter, attr_error_cnt = insert_similar_apps(root_app, counter,
-                                                           seed, app_cnt, attr_error_cnt)
-    return app_cnt, counter, attr_error_cnt
+def insert_app(app, seed, app_cnt, counter):
+    if app_not_already_exists(app):
+        defaults = get_app_details(app, seed, counter)
+        App.get_or_create(
+            app_id=app['app_id'],
+            defaults=defaults
+        )
+        app_cnt += 1
+        counter += 1
+    return app_cnt, counter
+
+
+def one_step_bfs(root_app, seed, app_cnt, counter):
+    app_cnt, counter = insert_app(root_app, seed, app_cnt, counter)
+    app_cnt, counter = insert_similar_apps(root_app, seed, app_cnt, counter)
+    return app_cnt, counter
+
+
+def proceed_in_graph(indicator, seed, app_cnt, counter, prev_count, last_update_time):
+    query = App.select().where(App.row_number == indicator)
+    for application in query:
+        root_app_id = application.app_id
+        while True:
+            try:
+                app = play_scraper.details(root_app_id)
+                break
+            except (ReadTimeout, ConnectionError):
+                root.warning("ReadTimeout error, waiting for 5 seconds.")
+                time.sleep(5)
+        app_cnt, counter = one_step_bfs(app, seed, app_cnt, counter)
+        prev_count, last_update_time = \
+        get_time_of_update(app_cnt, prev_count, last_update_time)
+    indicator += 1
+    return indicator, app_cnt, counter, prev_count, last_update_time
 
 
 def gather_data_for_seed(seed, counter, indicator, num_for_each_seed):
     app_cnt = 0
     prev_count = app_cnt
-    attr_error_cnt = 0
     last_update_time = datetime.datetime.now()
     while True:
         try:
             app = play_scraper.details(seed)
-            app_cnt, counter, attr_error_cnt = one_step_bfs(app,
-                                                            counter, seed, app_cnt,
-                                                            attr_error_cnt)
+            app_cnt, counter = one_step_bfs(app, seed, app_cnt, counter)
+            prev_count, last_update_time = \
+            get_time_of_update(app_cnt, prev_count, last_update_time)
             while app_cnt < num_for_each_seed:
                 if nothing_updated_for_long(last_update_time):
                     break
                 if indicator > counter:
                     break
-                query = App.select().where(App.row_number == indicator)
-                for application in query:
-                    root_app_id = application.app_id
-                    while True:
-                        try:
-                            app = play_scraper.details(root_app_id)
-                            break
-                        except (ReadTimeout, ConnectionError):
-                            root.warning("ReadTimeout error, waiting for 5 seconds.")
-                            time.sleep(5)
-                    app_cnt, counter, attr_error_cnt = one_step_bfs(app,
-                                                                    counter, seed,
-                                                                    app_cnt, attr_error_cnt)
-                    prev_count, last_update_time = get_time_of_update(app_cnt,
-                                                                      prev_count, last_update_time)
-                indicator += 1
+                indicator, app_cnt, counter, prev_count, last_update_time = \
+                    proceed_in_graph(indicator,
+                    seed, app_cnt, counter, prev_count, last_update_time)
             break
         except (ReadTimeout, ConnectionError):
             root.warning("ReadTimeout error, waiting for 5 seconds.")
@@ -137,7 +140,6 @@ def gather_data_for_seed(seed, counter, indicator, num_for_each_seed):
         except (HTTPError, ValueError):
             root.error("url for %s not found" % seed)
             break
-    attribute_error_list.append(attr_error_cnt)
     return counter
 
 
@@ -146,7 +148,7 @@ def crawl(seeds, num_for_each_seed):
     counter = 0
     indicator = 1
     for i in trange(num_seeds):
-        print("\n\nprocessing " + str(i)+"th seed " + str(seeds[i]) + ".\n\n")
+        print("\n\nprocessing " + str(i) + "th seed " + str(seeds[i]) + ".\n\n")
         if i > 0:
             indicator = counter
         counter = gather_data_for_seed(seeds[i], counter, indicator, num_for_each_seed)
@@ -157,7 +159,5 @@ db.create_tables([App, Similarity])
 
 conf = toml.load('config.toml')
 seeds = conf['seed_apps']
-AttributeErrorCounts = conf['AttributeErrorCounts_path']
 
 crawl(seeds, 1000)
-write_attribute_error_list(AttributeErrorCounts)
